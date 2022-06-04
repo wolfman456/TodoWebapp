9ba12d2672bb53f0a0d46cb3091797d3192b863f
Paul Shook <pshook456@gmail.com>
2022-06-03 21:58:42 -0500

    database and login page created

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    class ToDo(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(140))
+        description = db.Column(db.String(250))
+        completed = db.String(db.Boolean)
+        timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+        def __repr__(self):
+            return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -19,3 +20,13 @@ def index():
         }
     ]
     return render_template('index.html', title="home", user=user, todo_list=todo_list)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='sign in', form=form)
